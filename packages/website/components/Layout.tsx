6724a7a4fd8823357d4208b269e2916f279aac46
fox <fox@vvfox.com>
2020-02-29 08:18:38 +0800

    elevation scroll for toolbar

--- packages/website/components/Layout.tsx
+++ packages/website/components/Layout.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import Head from 'next/head';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Theme,
-  Container,
-
-} from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Theme, Container, useScrollTrigger } from '@material-ui/core';
 import { Footer } from './Footer';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { BottomNav } from './BottomNav';
@@ -20,13 +13,23 @@ import { useRouter } from 'next/router';
 import { LocaleContext } from '../src/context/LocaleContext';
 import { LocaleSwitcher } from './LocaleSwitcher';
 
-import {ContainerProps} from '@material-ui/core'
+import { ContainerProps } from '@material-ui/core';
 
 interface Props {
   title?: string;
   toolbar?: Component;
-  maxWidth?:  ContainerProps["maxWidth"];
+  maxWidth?: ContainerProps['maxWidth'];
 }
+
+const overrideToolbarStyle = makeStyles(
+  (theme: Theme) =>
+    createStyles({
+      root: {
+        background: theme.palette.primary.main,
+      },
+    }),
+  { name: 'MuiToolbar' }
+);
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     main: {
@@ -36,8 +39,30 @@ const useStyles = makeStyles((theme: Theme) =>
     grow: {
       flex: '1 1 auto',
     },
+
+    container:{
+      padding: 0,
+      margin: 0,
+    }
   })
 );
+
+const ElevationScroll: React.FC = ({ children }) => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: typeof window === 'undefined' ? undefined : window,
+  });
+
+  if (React.isValidElement(children)) {
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  } else {
+    console.error('children is not a valid react element');
+    return null;
+  }
+};
 
 const Layout: React.FunctionComponent<Props> = ({
   children,
@@ -45,6 +70,7 @@ const Layout: React.FunctionComponent<Props> = ({
   toolbar = null,
   maxWidth = 'md',
 }) => {
+  overrideToolbarStyle();
   const classes = useStyles();
   const { t, locale } = useTranslation();
 
@@ -60,17 +86,20 @@ const Layout: React.FunctionComponent<Props> = ({
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
-      <AppBar position="sticky">
-        <Toolbar>
-          <Typography>{title}</Typography>
-          {toolbar}
-          <div className={classes.grow}></div>
-          <LocaleSwitcher />
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar position="sticky">
+          <Toolbar>
+            {/* <Typography>{title}</Typography> */}
+            {toolbar}
+            <div className={classes.grow}></div>
+            <LocaleSwitcher />
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
 
       <main className={classes.main}>
-        <Container maxWidth={maxWidth}>{children}</Container>
+        <Container className={classes.container} maxWidth={maxWidth}>{children}</Container>
       </main>
       <footer>
         <Footer />
