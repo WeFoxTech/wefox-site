b559972c5041da7dde75d08543a48c5634e89c3f
fox <fox@vvfox.com>
2020-03-07 23:25:12 +0800

    add loading fox logo 💯

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
+import Fox from '~/components/svgs/fox';
 import { getInitialLocale } from '../src/translations/getInitialLocale';
+import { Container, Box, Typography } from '@material-ui/core';
 
 const Index: React.FC = () => {
   React.useEffect(() => {
     window.location.replace(`/${getInitialLocale()}`);
-  });
+  }, []);
 
   return (
-    <Head>
-      <meta name="robots" content="noindex, nofollow" />
-    </Head>
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <Box bgcolor="grey.800" height="100vh">
+        <Container maxWidth="sm">
+          <Box pt={10} textAlign="center" color="grey.50">
+            <Fox />
+            <Typography variant="h1">WeFox</Typography>
+          </Box>
+        </Container>
+      </Box>
+    </>
   );
 };
 
