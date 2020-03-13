25da89acb0c8f315aea22dd978be6f6fba056f77
fox <fox@vvfox.com>
2020-03-14 00:01:16 +0800

    cahnge bgcolor for entry page 💯

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -6,13 +6,13 @@ import { Container, Box, Typography } from '@material-ui/core';
 import { useRouter } from 'next/router';
 import { languageNames, localeNames, Locale } from '~/src/translations/config';
 import Link from '~/src/Link';
+import theme from '~/src/theme';
 
 const Index: React.FC = () => {
   const router = useRouter();
   React.useEffect(() => {
     const locale = getInitialLocale();
     router.push('/[lang]/', `/${locale}/`);
-    // window.location.replace(`/${locale}`);
   }, []);
 
   return (
@@ -20,20 +20,26 @@ const Index: React.FC = () => {
       <Head>
         <meta name="robots" content="noindex, nofollow" />
       </Head>
-      <Box bgcolor="grey.400" height="100vh">
+      <Box bgcolor={theme.palette.primary.main} minHeight="100vh">
         <Container maxWidth="sm">
           <Box pt={10} px={8} textAlign="center" color="grey.50">
             <Fox />
             <Typography variant="h6">Detecting language for you...</Typography>
             <Typography variant="h1">WeFox</Typography>
-            <Typography variant="h5" color="error" component="noscript">
-              The current environment does not have a script environment, please select the language
-              manually
+            <Typography variant="h5" color="textPrimary" component="noscript">
+              The current environment is no script environment, manually select a language
             </Typography>
           </Box>
           {localeNames.map(l => (
             <Box textAlign="center" py={1}>
-              <Link variant="h5" key={l} lang={l} href={`/[lang]/`} as={`/${l}/`}>
+              <Link
+                variant="h5"
+                color="textPrimary"
+                key={l}
+                lang={l}
+                href={`/[lang]/`}
+                as={`/${l}/`}
+              >
                 {languageNames[l]}
               </Link>
             </Box>
