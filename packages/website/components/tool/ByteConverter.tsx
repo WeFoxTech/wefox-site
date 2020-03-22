afd1d7140ff56b88a61818e54ee09fcfd3406763
fox <fox@vvfox.com>
2020-03-22 20:42:07 +0800

    add decimal unit :release

--- packages/website/components/tool/ByteConverter.tsx
+++ packages/website/components/tool/ByteConverter.tsx
@@ -19,34 +19,111 @@ import {
   InputAdornment,
   FormControl,
   Input,
+  Tooltip,
 } from '@material-ui/core';
 import BigNumber from 'bignumber.js';
 import { TranslationKey, InlineLocale } from '~/src/translations/types';
 import copy from 'copy-to-clipboard';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import Alert from '@material-ui/lab/Alert';
+import Link from '~/src/Link';
+import HelpIcon from '@material-ui/icons/Help';
 
 const title: InlineLocale = {
   zh: '字节单位换算工具',
   en: 'Byte Unit Conversion Tool',
 };
 
+interface DataItem {
+  v: number;
+  name: string;
+}
+
 const units = {
-  bit: 1,
-  Byte: 8,
-  KiB: 2 ** 10 * 8,
-  MiB: 2 ** 20 * 8,
-  GiB: 2 ** 30 * 8,
-  TiB: 2 ** 40 * 8,
-  PiB: 2 ** 50 * 8,
-  EiB: 2 ** 60 * 8,
-  ZiB: 2 ** 70 * 8,
-  YiB: 2 ** 80 * 8,
+  b: {
+    v: 1,
+    name: 'bit',
+  },
+  B: {
+    v: 8,
+    name: 'Byte',
+  },
+  Ki: {
+    v: 2 ** 10 * 8,
+    name: 'kibi',
+  },
+  Mi: {
+    v: 2 ** 20 * 8,
+    name: 'mebi',
+  },
+  Gi: {
+    v: 2 ** 30 * 8,
+    name: 'gibi',
+  },
+  Ti: {
+    v: 2 ** 40 * 8,
+    name: 'tebi',
+  },
+  Pi: {
+    v: 2 ** 50 * 8,
+    name: 'pebi',
+  },
+  Ei: {
+    v: 2 ** 60 * 8,
+    name: 'exbi',
+  },
+  Zi: {
+    v: 2 ** 70 * 8,
+    name: 'zebi',
+  },
+  Yi: {
+    v: 2 ** 80 * 8,
+    name: 'yobi',
+  },
+};
+
+const decimals = {
+  k: {
+    v: 1000,
+    name: 'kilo',
+  },
+  M: {
+    v: 1000 ** 2,
+    name: 'mega',
+  },
+  G: {
+    v: 1000 ** 3,
+    name: 'giga',
+  },
+  T: {
+    v: 1000 ** 4,
+    name: 'tera',
+  },
+  P: {
+    v: 1000 ** 5,
+    name: 'peta',
+  },
+  E: {
+    v: 1000 ** 6,
+    name: 'exa',
+  },
+  Z: {
+    v: 1000 ** 7,
+    name: 'zetta',
+  },
+  Y: {
+    v: 1000 ** 8,
+    name: 'yotta',
+  },
 };
 
 type Units = typeof units;
 
 type UnitKey = keyof Units;
+
+type Decimal = typeof decimals;
+
+type DecimalKey = keyof Decimal;
 
 export const ByteConverter: React.FC = () => {
   const { t, locale } = useTranslation();
@@ -82,7 +159,7 @@ export const ByteConverter: React.FC = () => {
     }
   };
 
-  const copyText = (name: UnitKey, text: string) => {
+  const copyText = (name: UnitKey | DecimalKey, text: string) => {
     const result = copy(text);
     if (result) {
       setSuccess(t('copySuccess'));
@@ -94,15 +171,45 @@ export const ByteConverter: React.FC = () => {
   const data: Array<[
     UnitKey,
     string,
-    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+    DataItem
   ]> = Object.keys(units).map(ele => {
     const key = ele as UnitKey;
+    const item = units[key];
+    const { v, name } = item;
     return [
       key,
-      fixedType[key] ??
-        (key === 'bit' ? ns?.toFixed(0) ?? '' : ns?.dividedBy(units[key]).toString(10) ?? ''),
+      fixedType[key] ?? (key === 'b' ? ns?.toFixed(0) ?? '' : ns?.dividedBy(v).toString(10) ?? ''),
       (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
-        checkAndSetNs(key, e.currentTarget.value, bn => bn.multipliedBy(units[key])),
+        checkAndSetNs(key, e.currentTarget.value, bn => bn.multipliedBy(v)),
+      item,
+    ];
+  });
+
+  const decimalData: Array<[
+    DecimalKey,
+    string,
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+    DataItem
+  ]> = Object.keys(decimals).map(ele => {
+    const key = ele as DecimalKey;
+    const item = decimals[key];
+    const { v, name } = item;
+    let byteValue = fixedType[key];
+    if (!byteValue) {
+      if (ns) {
+        byteValue = new BigNumber(ns.dividedBy(8).toFixed()).dividedBy(v).toString(10);
+      } else {
+        byteValue = '';
+      }
+    }
+
+    return [
+      key,
+      byteValue,
+      (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+        checkAndSetNs(key, e.currentTarget.value, bn => bn.multipliedBy(v).multipliedBy(8)),
+      item,
     ];
   });
 
@@ -127,7 +234,13 @@ export const ByteConverter: React.FC = () => {
         </Alert>
       </Snackbar>
       <Box textAlign="center" justifyContent="center">
-        <Typography variant="h3">{title[locale]}</Typography>
+        <Typography variant="h3">
+          {title[locale]}
+          <Link href="https://en.wikipedia.org/wiki/Byte" target="_blank">
+            <HelpIcon />
+          </Link>
+        </Typography>
+
         <Box pt={4}>
           <Button
             color="primary"
@@ -145,14 +258,14 @@ export const ByteConverter: React.FC = () => {
         <form>
           <Grid container spacing={1} justify="center">
             {data.map((e, i) => {
-              const [k, v, h] = e;
+              const [k, v, h, item] = e;
               return (
                 <Grid key={i} item xs={12} md={6}>
                   <Box>
                     <FormControl>
-                      <InputLabel htmlFor={`byte-unit-${k}`}>{k}</InputLabel>
+                      <InputLabel htmlFor={`binary-unit-${k}`}>{item.name}</InputLabel>
                       <Input
-                        id={`byte-unit-${k}`}
+                        id={`binary-unit-${k}`}
                         value={v}
                         onChange={h}
                         fullWidth
@@ -160,10 +273,48 @@ export const ByteConverter: React.FC = () => {
                         multiline
                         endAdornment={
                           <InputAdornment position="end">
+                            <Typography>{k}</Typography>
                             {v && (
-                              <Button startIcon={<FileCopyIcon />} onClick={e => copyText(k, v)}>
-                                {t('copy')}
-                              </Button>
+                              <Tooltip title={t('copy')}>
+                                <IconButton onClick={e => copyText(k, v)}>
+                                  <FileCopyIcon />
+                                </IconButton>
+                              </Tooltip>
+                            )}
+                          </InputAdornment>
+                        }
+                      ></Input>
+                    </FormControl>
+                  </Box>
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Box py={4}></Box>
+          <Grid container spacing={1} justify="center">
+            {decimalData.map((e, i) => {
+              const [k, v, h, item] = e;
+              return (
+                <Grid key={i} item xs={12} md={6}>
+                  <Box>
+                    <FormControl>
+                      <InputLabel htmlFor={`decimal-unit-${k}`}>{item.name}</InputLabel>
+                      <Input
+                        id={`decimal-unit-${k}`}
+                        value={v}
+                        onChange={h}
+                        fullWidth
+                        type="number"
+                        multiline
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <Typography>{k}</Typography>
+                            {v && (
+                              <Tooltip title={t('copy')}>
+                                <IconButton onClick={e => copyText(k, v)}>
+                                  <FileCopyIcon />
+                                </IconButton>
+                              </Tooltip>
                             )}
                           </InputAdornment>
                         }
