79e15c9ab9fb775558b8822df3df3f54a9c6ce4f
fox <fox@vvfox.com>
2020-04-16 17:11:45 +0800

    add padding for post layout

--- packages/website/src/hocs/withPost.tsx
+++ packages/website/src/hocs/withPost.tsx
@@ -6,7 +6,7 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../../src/theme';
-import { Container, Paper } from '@material-ui/core';
+import { Container, Paper, Box } from '@material-ui/core';
 import { LocaleProvider } from '../context/LocaleContext';
 import { Locale } from '../translations/config';
 import { isLocale, Translations, Namespace } from '../translations/types';
@@ -15,7 +15,11 @@ import Layout from '~/components/Layout';
 import { PageMeta } from '../PageMeta';
 
 const PostLayout: React.FC<{ meta: PageMeta }> = ({ meta, children }) => {
-  return <Layout meta={meta}>{children}</Layout>;
+  return (
+    <Layout meta={meta}>
+      <Box px={2}>{children}</Box>
+    </Layout>
+  );
 };
 
 export default (meta: PageMeta): React.FC => ({ children }) => {
