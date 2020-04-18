514820671a7ca66a28a2586afa3e29c8c7aff90a
fox <fox@vvfox.com>
2020-04-18 08:49:05 +0800

    global blockquote style

--- packages/website/components/globalStyle.tsx
+++ packages/website/components/globalStyle.tsx
@@ -4,6 +4,12 @@ export default () => (
       .text-with-icon {
         vertical-align: top;
       }
+      blockquote {
+        padding: 0 1em;
+        color: #6a737d;
+        border-left: 0.25em solid #dfe2e5;
+        margin-bottom: 16px;
+      }
     `}
   </style>
 );
